91554560ad2f1a9da43662e3c943055e56f489b6
Jiho Park <bapjoo2@gmail.com>
2018-11-22 00:27:43 +0900

    Updates

---
+++ precache-manifest.b8d8127969c5fe0be8dd7bdd097541c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "7c076293cb340294ba48",
+    "url": "/movie_app/static/js/main.7c076293.chunk.js"
+  },
+  {
+    "revision": "d0c28ac72245ff7380f5",
+    "url": "/movie_app/static/js/1.d0c28ac7.chunk.js"
+  },
+  {
+    "revision": "7c076293cb340294ba48",
+    "url": "/movie_app/static/css/main.13183e5a.chunk.css"
+  },
+  {
+    "revision": "21d65365aaaee1a7a90b3a6f91e90fa2",
+    "url": "/movie_app/index.html"
+  }
+];
